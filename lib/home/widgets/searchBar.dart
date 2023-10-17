e6c6ce6c5a091885c367b3aa11c6bbdeb2cf646c
Pudv95 <vivekupadhayay18@gmail.com>
2023-10-17 22:22:16 +0530

    resolve searchbar bug

--- lib/home/widgets/searchBar.dart
+++ lib/home/widgets/searchBar.dart
@@ -12,34 +12,37 @@ class MySearchBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Hero(
       tag: tag,
-      child: TextField(
-        controller: searchController,
-        onSubmitted: (query) {
-          SearchModel temp1 = SearchModel(title: 'HelloWorld', link: 'HelloWorld', snippet: 'HelloWorld', formattedURL: 'HelloWorld', image: 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg');
-          SearchModel temp2 = SearchModel(title: 'HelloWorld', link: 'HelloWorld', snippet: 'HelloWorld', formattedURL: 'HelloWorld', image: 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg');
-          SearchModel temp3 = SearchModel(title: 'HelloWorld', link: 'HelloWorld', snippet: 'HelloWorld', formattedURL: 'HelloWorld', image: 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg');
-          SearchModel temp4 = SearchModel(title: 'HelloWorld', link: 'HelloWorld', snippet: 'HelloWorld', formattedURL: 'HelloWorld', image: 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg');
-          List<SearchModel> searchList = [temp1,temp2,temp3,temp4];
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SearchResult(searchResults: searchList,searchController: searchController,)),
-          );
-        },
-        style: const TextStyle(color: Colors.white),
-        decoration:  InputDecoration(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(50),borderSide: BorderSide(color: Colors.white)),
-          prefixIcon: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SvgPicture.asset('assets/Icons/search-icon.svg',colorFilter: const ColorFilter.mode(Colors.black54, BlendMode.srcIn),),
+      child: Material(
+      type: MaterialType.transparency,
+        child: TextField(
+          controller: searchController,
+          onSubmitted: (query) {
+            SearchModel temp1 = SearchModel(title: 'HelloWorld', link: 'HelloWorld', snippet: 'HelloWorld', formattedURL: 'HelloWorld', image: 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg');
+            SearchModel temp2 = SearchModel(title: 'HelloWorld', link: 'HelloWorld', snippet: 'HelloWorld', formattedURL: 'HelloWorld', image: 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg');
+            SearchModel temp3 = SearchModel(title: 'HelloWorld', link: 'HelloWorld', snippet: 'HelloWorld', formattedURL: 'HelloWorld', image: 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg');
+            SearchModel temp4 = SearchModel(title: 'HelloWorld', link: 'HelloWorld', snippet: 'HelloWorld', formattedURL: 'HelloWorld', image: 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg');
+            List<SearchModel> searchList = [temp1,temp2,temp3,temp4];
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SearchResult(searchResults: searchList,searchController: searchController,)),
+            );
+          },
+          style: const TextStyle(color: Colors.white),
+          decoration:  InputDecoration(
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(50),borderSide: BorderSide(color: Colors.white)),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset('assets/Icons/search-icon.svg',colorFilter: const ColorFilter.mode(Colors.black54, BlendMode.srcIn),),
+            ),
+            suffixIcon: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset('assets/Icons/mic-icon.svg',),
+            ),
+            suffixIconColor: Colors.white,
+            prefixIconColor: Colors.white,
+            focusedBorder: OutlineInputBorder( borderRadius: BorderRadius.circular(50),borderSide: const BorderSide(color: Colors.white70)),
+            enabledBorder: OutlineInputBorder( borderRadius: BorderRadius.circular(50),borderSide: const BorderSide(color: Colors.white12)),
           ),
-          suffixIcon: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SvgPicture.asset('assets/Icons/mic-icon.svg',),
-          ),
-          suffixIconColor: Colors.white,
-          prefixIconColor: Colors.white,
-          focusedBorder: OutlineInputBorder( borderRadius: BorderRadius.circular(50),borderSide: const BorderSide(color: Colors.white70)),
-          enabledBorder: OutlineInputBorder( borderRadius: BorderRadius.circular(50),borderSide: const BorderSide(color: Colors.white12)),
         ),
       ),
     );
